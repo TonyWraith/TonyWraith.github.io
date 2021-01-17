754b32b885c979d7dc232d9f489f59998e9a49d0
TonyWraith <72389455+TonyWraith@users.noreply.github.com>
2021-01-17 20:20:03 +0300

    Файлы для сайта

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html> 
+<html lang='ru'> 
+<head> 
+<title>Гостиница "Престиж"</title> 
+<meta charset='utf-8'> 
+<link rel="stylesheet" href="style/style.css">
+<link rel="icon" type="image/png" sizes="192x192" href="image/n1.png"> 
+</head>
+<body>
+<header id="header" class="header">
+<!-- 
+LOGO
+<img src="image/logo.png" alt="logo" width="40%">
+ -->
+	<div  class="name">ГОСТИНИЦА <br> "ПРЕСТИЖ"</div>
+		<ul class="menu">
+			<li><a href="#">Номера</a></li>
+			<li><a href="#">Отзывы</a></li>
+			<li><a href="#">О нас</a></li>
+			<li><a href="#">Акции</a></li>
+		</ul>
+	<div class="about">
+		<p>+7(777) 777-77-77 <br>г.Улан-Удэ ул.ХХХ д. ХХ</p>
+	</div>
+</header>
+<article id="bron">
+	<div  id="pic1">
+		<img src="image/glav.jpg" alt="ГЛАВНОЕ ФОТО" width="100%" height="880px">
+	</div>
+	<div  id="name">
+		<h2>Бронирование номера<h2>
+	 </div>
+	 <div id="left-bron">
+		<h2>Заезд</h2>
+	 </div>
+	 <div id="right-bron">
+		<h2>Выезд</h2>
+	 </div>
+</article>
+<footer>
+</footer>
+</body> 
+</html>
